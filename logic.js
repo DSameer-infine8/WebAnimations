2fee357047b4566e9b0e8c07e183d0c29e41e03b
SamInfine8 <sameer8creative.tech@gmail.com>
2025-07-09 23:38:14 +0530

    NavBar and Title Card Animations

--- logic.js
+++ logic.js
@@ -1,17 +1,34 @@
-gsap.from(".ok",{
+const tl = gsap.timeline();
+
+tl.from(".name",{
+    y:-10,
     opacity:0,
-    duration:2,
-    y:40,
-    delay:1,
-    stagger:-1
+    duration:0.77,
+    delay:1
 });
 
-gsap.to(".box",{
-    x:300,
+tl.from("h2",{
+    y:-10,
+    opacity:0,
+    duration:0.5,
+    stagger:0.5
+});
+
+tl.from(".hmm",{
+    opacity:0,
+    fontSize:0,
     duration:1,
-    delay:1,
-    rotate:360,
-    repeat:-1,  //for infinte give value -1
-    yoyo:true,
-    borderRadius: "50%"
+    stagger:0.5
+});
+
+tl.from(".show",{
+    y:-50,
+    opacity:0,
+    duration:1.5,
+});
+
+tl.from(".project",{
+    height:0,
+    width: 0,
+    duration:1,
 })
