278dfe5c66158b428a31e563d68c6905bd6704df
SamInfine8 <sameer8creative.tech@gmail.com>
2025-07-09 21:06:08 +0530

    Basic Animations

--- logic.js
+++ logic.js
@@ -1,7 +1,17 @@
-// gsap.from(".box",{
+gsap.from(".ok",{
+    opacity:0,
+    duration:2,
+    y:40,
+    delay:1,
+    stagger:-1
+});
 
-// });
-
-// gsap.to(".box",{
-
-// });
+gsap.to(".box",{
+    x:300,
+    duration:1,
+    delay:1,
+    rotate:360,
+    repeat:-1,  //for infinte give value -1
+    yoyo:true,
+    borderRadius: "50%"
+})
